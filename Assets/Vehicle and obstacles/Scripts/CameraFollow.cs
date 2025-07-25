52988955bbaac7706a2622cfd2896cb7f0c340a0
NNish <nnish@192.168.1.8>
2025-07-25 15:50:47 +0300

    Change camera view

--- Assets/Vehicle and obstacles/Scripts/CameraFollow.cs
+++ Assets/Vehicle and obstacles/Scripts/CameraFollow.cs
@@ -6,11 +6,14 @@ public class CameraFollow : MonoBehaviour
     public PlayerController player2;
     private PlayerController currentView;
     private Vector3 offset;
+    private Vector3 nearOffset;
+    private Vector3 tempOffset;
     private void Start()
     {
         currentView = player1;
         offset = transform.position;
-
+        nearOffset = new Vector3(0f, 2f, 0.5f);
+        tempOffset = offset;
     }
     void LateUpdate()
     {
@@ -18,7 +21,22 @@ public class CameraFollow : MonoBehaviour
         {
             ChangeVehicle();
         }
-        transform.position = currentView.transform.position + offset;
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            ChangeCameraVeiw();
+        }
+        transform.position = currentView.transform.position + tempOffset;
+    }
+    private void ChangeCameraVeiw()
+    {
+        if (tempOffset == offset)
+        {
+            tempOffset = nearOffset;
+        }
+        else
+        {
+            tempOffset = offset;
+        }
     }
     private void ChangeVehicle()
     {
