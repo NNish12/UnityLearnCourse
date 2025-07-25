bab2e9bfee2e39fce2a8b30554bb5b020f601d35
NNish <nnish@192.168.1.8>
2025-07-25 23:48:47 +0300

    Fix SpawnManager script

--- Assets/FeedAnimals/Scripts/SpawnManager.cs
+++ Assets/FeedAnimals/Scripts/SpawnManager.cs
@@ -5,14 +5,20 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private float spawnRangeX = 20f;
     [SerializeField] private float spawnPosZ = 20f;
     [SerializeField] private GameObject[] animalPrefabs;
+    [SerializeField] private float startDelay = 2f;
+    [SerializeField] private float spawnInterval = 1.5f;
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
     private void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int index = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3 (Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            Instantiate(animalPrefabs[index], spawnPos, animalPrefabs[index].transform.rotation);
-        }
+    }
+    private void SpawnRandomAnimal()
+    {
+        int index = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[index], spawnPos, animalPrefabs[index].transform.rotation);
     }
 }
