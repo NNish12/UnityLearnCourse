ccefdd1d4f49476a919d800d74b3650ad51c0bf8
NNish <nnish@192.168.1.8>
2025-07-25 20:38:22 +0300

    Scropt destroy objects out of scene

---
+++ Assets/FeedAnimals/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    [SerializeField] private float topBound = 30f;
+    [SerializeField] private float bottomBound = -10f;
+    private void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(this.gameObject);
+        }
+        else if (transform.position.z < bottomBound)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
