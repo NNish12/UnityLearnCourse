af9ea74a75425942a1b32fd58ed473a4ac5571bf
NNish <nnish@192.168.1.8>
2025-07-27 21:33:38 +0300

    ModeTheCubeProject

--- Assets/Vehicle and obstacles/Scripts/CameraFollow.cs
+++ Assets/Vehicle and obstacles/Scripts/CameraFollow.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public PlayerController player1;
-    public PlayerController player2;
     private PlayerController currentView;
     private Vector3 offset;
     private Vector3 nearOffset;
     private Vector3 tempOffset;
-    [SerializeField] private float speedCameraRotate = 15f;
     private void Start()
     {
         currentView = player1;
@@ -19,16 +17,11 @@ public class CameraFollow : MonoBehaviour
     }
     void LateUpdate()
     {
-        if (Input.GetKeyUp(KeyCode.R))
-        {
-            ChangeVehicle();
-        }
         if (Input.GetKeyUp(KeyCode.Q))
         {
             ChangeCameraVeiw();
         }
         transform.position = currentView.transform.position + tempOffset;
-        transform.RotateAround(currentView.transform.position, Vector3.up, currentView.GetHorizontalInput() * Time.deltaTime * speedCameraRotate);
 
     }
     private void ChangeCameraVeiw()
@@ -42,23 +35,4 @@ public class CameraFollow : MonoBehaviour
             tempOffset = offset;
         }
     }
-    private void ChangeVehicle()
-    {
-        if (currentView == player1)
-        {
-            SetActivePlayer(player2, player1);
-        }
-        else
-        {
-            SetActivePlayer(player1, player2);
-        }
-
-    }
-    private void SetActivePlayer(PlayerController activePlayer, PlayerController inactivePlayer)
-    {
-        activePlayer.enabled = true;
-        inactivePlayer.enabled = false;
-        currentView = activePlayer;
-    }
-
 }
